5c7515c4666024177ba9453a25607f7b0f21cf1c
Renan Oliveira <renandemelo@gmail.com>
2013-03-21 09:23:08 -0300

    colocando multiplicacao

--- web.rb
+++ web.rb
@@ -11,3 +11,11 @@ get '/soma' do
   "Resultado: #{result}"
 end
 
+get '/multiplica' do
+  cal = Calculadora.new
+  a = params["a"].to_i
+  b = params["b"].to_i
+  result = cal.multiplica(a,b)
+  "Resultado: #{result}"
+end
+
