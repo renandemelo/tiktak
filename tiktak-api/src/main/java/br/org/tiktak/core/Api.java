cc8ea6e18cbaf2fbbdb11a75df8943d790615885
Albert De La Fuente <vonpupp@gmail.com>
2013-03-26 15:59:45 -0300

    Limpeza do codigo

--- tiktak-api/src/main/java/br/org/tiktak/core/Api.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/Api.java
@@ -9,7 +9,6 @@ public class Api {
 	public static void registrarEvento(String usuario, String evento){
 		Dados dados = new Dados(usuario, evento);
 		Gson gson = new Gson();
-		//gson.toJson(dados);
 		String json = gson.toJson(dados);
 		System.out.println(json);
 		
@@ -20,16 +19,9 @@ public class Api {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-		//new GsonBuilder().setPrettyPrinting().create();
-		//gson.
-//		Writer data = new OutputStreamWriter(
-//			        Main.class.getResourceAsStream("books.json"), "UTF-8");
-		// Api a = new Api('usuario', 'evento');
 	}
 	
 	public static void main(String[] args){
-		//Api a = new Api();
 		Api.registrarEvento("albert", "cadastro");
 	}
 }
