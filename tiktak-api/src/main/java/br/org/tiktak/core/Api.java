25f59e817b75ea0004c51ad870a96245cad5be6c
Albert De La Fuente <vonpupp@gmail.com>
2013-03-26 15:58:09 -0300

    O API ja cria o arquivo tik.tak, e imprime o resultado do json como saida

--- tiktak-api/src/main/java/br/org/tiktak/core/Api.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/Api.java
@@ -1,5 +1,8 @@
 package br.org.tiktak.core;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 
 public class Api {
@@ -7,11 +10,21 @@ public class Api {
 		Dados dados = new Dados(usuario, evento);
 		Gson gson = new Gson();
 		//gson.toJson(dados);
-		System.out.println(gson.toJson(dados));
+		String json = gson.toJson(dados);
+		System.out.println(json);
+		
+        try {
+            FileWriter writer = new FileWriter("tik.tak");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
 		//new GsonBuilder().setPrettyPrinting().create();
 		//gson.
-		//Writer data = new OutputStreamWriter(
-		//	        Main.class.getResourceAsStream("books.json"), "UTF-8");
+//		Writer data = new OutputStreamWriter(
+//			        Main.class.getResourceAsStream("books.json"), "UTF-8");
 		// Api a = new Api('usuario', 'evento');
 	}
 	
