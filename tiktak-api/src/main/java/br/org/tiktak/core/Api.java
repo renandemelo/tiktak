7b39a87c450e18b10d550dd0c68c84aa014517d0
Albert De La Fuente <vonpupp@gmail.com>
2013-03-26 15:50:00 -0300

    foi separada a representaçao dos dados e a chamada a API em duas classes

--- tiktak-api/src/main/java/br/org/tiktak/core/Api.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/Api.java
@@ -1,35 +1,22 @@
 package br.org.tiktak.core;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class Api {
-	String usuario;
-	String evento;
-
-	public Api(String usuario, String evento){
-		this.usuario = usuario;
-		this.evento = evento;
-		
+	public static void registrarEvento(String usuario, String evento){
+		Dados dados = new Dados(usuario, evento);
 		Gson gson = new Gson();
-		gson.toJson(usuario);
-		gson.toJson(evento);
-		System.out.println(gson.toJson());
+		//gson.toJson(dados);
+		System.out.println(gson.toJson(dados));
 		//new GsonBuilder().setPrettyPrinting().create();
 		//gson.
 		//Writer data = new OutputStreamWriter(
+		//	        Main.class.getResourceAsStream("books.json"), "UTF-8");
+		// Api a = new Api('usuario', 'evento');
 	}
 	
-//	public static void registrarEvento(String usuario, String evento){
-//		Gson gson = new Gson();
-//		gson.
-//		new GsonBuilder().setPrettyPrinting().create();
-//		//gson.
-//		//Writer data = new OutputStreamWriter(
-//		//	        Main.class.getResourceAsStream("books.json"), "UTF-8");
-//		// Api a = new Api('usuario', 'evento');
-//	}
 	public static void main(String[] args){
-		Api a = new Api("usuario", "evento");
+		//Api a = new Api();
+		Api.registrarEvento("albert", "cadastro");
 	}
 }
