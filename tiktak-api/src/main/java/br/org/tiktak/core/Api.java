3de7a804fa3b3867578b163f4d3693ece7275022
Albert De La Fuente <vonpupp@gmail.com>
2013-03-26 15:33:59 -0300

    primera tentativa de converter a json usando objeto

--- tiktak-api/src/main/java/br/org/tiktak/core/Api.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/Api.java
@@ -1,8 +1,35 @@
 package br.org.tiktak.core;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Api {
-	
-	public static void registrarEvento(String usuario, String evento){
+	String usuario;
+	String evento;
+
+	public Api(String usuario, String evento){
+		this.usuario = usuario;
+		this.evento = evento;
 		
+		Gson gson = new Gson();
+		gson.toJson(usuario);
+		gson.toJson(evento);
+		System.out.println(gson.toJson());
+		//new GsonBuilder().setPrettyPrinting().create();
+		//gson.
+		//Writer data = new OutputStreamWriter(
+	}
+	
+//	public static void registrarEvento(String usuario, String evento){
+//		Gson gson = new Gson();
+//		gson.
+//		new GsonBuilder().setPrettyPrinting().create();
+//		//gson.
+//		//Writer data = new OutputStreamWriter(
+//		//	        Main.class.getResourceAsStream("books.json"), "UTF-8");
+//		// Api a = new Api('usuario', 'evento');
+//	}
+	public static void main(String[] args){
+		Api a = new Api("usuario", "evento");
 	}
 }
