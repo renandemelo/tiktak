3c66c92601a9419df857fca1fa7da8eca0b6d6f4
Roberto Rodrigues <me@rrodrigues.net>
2013-04-10 22:47:00 -0300

    arrumando refatoração que foi feita no tiktak-api e refletida no dashboard

--- tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/Core.java
+++ tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/Core.java
@@ -4,32 +4,32 @@ import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
 
-//import br.org.tiktak.core.Dados;
+import br.org.tiktak.core.Evento;
 import br.org.tiktak.core.GsonFactory;
 
 import com.google.gson.reflect.TypeToken;
 
-
 /**
  * 
  * @author roberto.rodrigues
- *
+ * 
  */
 public class Core {
 	private String funcionalidade;
-	
+
 	private Integer quantidadeUsadaDaFuncionalidade;
-	
-	private HashMap<String, Integer> funcionalidadePorQuantidade = new HashMap<String, Integer>();
-	
-	public String getFuncionaidade(){
+
+	private final HashMap<String, Integer> funcionalidadePorQuantidade = new HashMap<String, Integer>();
+
+	public String getFuncionaidade() {
 		return this.funcionalidade;
 	}
-	
-//	public List<Dados> lerArquivo(FileReader arquivo ){
-//		List<Dados> lista = GsonFactory.getGson().fromJson(arquivo, new TypeToken<List<Dados>>() {}.getType());
 
-//		return lista;
-//	}
-	
+	public List<Evento> lerArquivo(final FileReader arquivo) {
+		List<Evento> lista = GsonFactory.getGson().fromJson(arquivo, new TypeToken<List<Evento>>() {
+		}.getType());
+
+		return lista;
+	}
+
 }
--- tiktak-dashboard/core/src/test/java/br/org/tiktak/dashboard/core/CoreTest.java
+++ tiktak-dashboard/core/src/test/java/br/org/tiktak/dashboard/core/CoreTest.java
@@ -1,31 +1,24 @@
 package br.org.tiktak.dashboard.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 
-import br.org.tiktak.core.Api;
-//import br.org.tiktak.core.Dados;
-import br.org.tiktak.core.GsonFactory;
-
-import com.google.gson.reflect.TypeToken;
+import br.org.tiktak.core.Evento;
 
 public class CoreTest {
 
 	@Test
-	public void testeLerArquivo() throws FileNotFoundException {		
+	public void testeLerArquivo() throws FileNotFoundException {
 		Core core = new Core();
 		FileReader arquivo = new FileReader("../../modeloDeArquivo/tik.tak");
-//		List<Dados> listaDados = core.lerArquivo(arquivo);
-		
-//		assertEquals("ROBERTO", listaDados.get(3).getUsuario());
+		List<Evento> listaEventos = core.lerArquivo(arquivo);
+
+		assertEquals("ROBERTO", listaEventos.get(3).getUsuario());
 	}
-	
 
 }
