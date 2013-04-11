3ddbb75faee4085077d8dfa49f2d07650fa1c93a
RobertoRodrigues <roberto.rodrigues@usp.br>
2013-04-11 14:45:39 -0300

    Implementando versao dinamica da tabela no dashboard com varias linhas

--- tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/BDfuncionalidades.java
+++ tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/BDfuncionalidades.java
@@ -2,8 +2,25 @@ package br.org.tiktak.dashboard.core;
 
 public class BDfuncionalidades {
 	private String funcionalidade;
-	public BDfuncionalidades(String funcionalidade, int i, int j) {
+	private int quantidade;
+	private double porcentagem;
+	
+	public BDfuncionalidades(String funcionalidade, int quantidade, int porcentagem) {
 		this.funcionalidade = funcionalidade;
+		this.quantidade = quantidade;
+		this.porcentagem = porcentagem; 
+	}
+	
+	public double getPorcetagem() {
+		return porcentagem;
+	}
+
+	public void setPorcetagem(double porcetagem) {
+		this.porcentagem = porcetagem;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
 	}
 
 	public String getFuncionalidade() {
--- tiktak-dashboard/web/src/main/java/br/org/tiktak/dashboard/pages/tabela/Tabela.java
+++ tiktak-dashboard/web/src/main/java/br/org/tiktak/dashboard/pages/tabela/Tabela.java
@@ -6,9 +6,10 @@ import java.util.List;
 import jmine.tec.web.wicket.pages.Template;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 
 import bancosys.tec.exception.MessageCreator;
-import br.org.tiktak.core.Evento;
 import br.org.tiktak.dashboard.core.BDfuncionalidades;
 
 public class Tabela extends Template {
@@ -16,7 +17,7 @@ public class Tabela extends Template {
 	String usuario;
 	String funcionalidade;
 
-	List<BDfuncionalidades> tabela = new ArrayList<BDfuncionalidades>();
+	List<BDfuncionalidades> funcionalidades = new ArrayList<BDfuncionalidades>();
 
 	@Override
 	protected MessageCreator getHelpTextCreator() {
@@ -27,11 +28,26 @@ public class Tabela extends Template {
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		tabela.add(new BDfuncionalidades("Tela 1", 1, 100));
-		for (BDfuncionalidades e : tabela) {
-			add(new Label("funcionalidade", e.getFuncionalidade()));
-			add(new Label("quantidade", "1"));
-			add(new Label("porcentagem", "100%"));
-		}
+		preencheListaFake();
+		ListView<BDfuncionalidades> listView = new ListView<BDfuncionalidades>("lvFuncionalidades") {
+
+			@Override
+			protected void populateItem(ListItem<BDfuncionalidades> item) {
+				item.add(new Label("funcionalidade", item.getModelObject().getFuncionalidade()));				
+				item.add(new Label("quantidade", item.getModelObject().getQuantidade() + ""));				
+				item.add(new Label("porcentagem", item.getModelObject().getPorcetagem() + "%"));				
+			}
+			
+		};
+		
+		listView.setList(funcionalidades);
+		this.add(listView);
 	}
+	
+	private void preencheListaFake(){
+		funcionalidades.add(new BDfuncionalidades("Tela 1", 2, 20));
+		funcionalidades.add(new BDfuncionalidades("Tela 2", 5, 50));
+		funcionalidades.add(new BDfuncionalidades("Tela 3", 3, 30));
+	}
+	
 }
