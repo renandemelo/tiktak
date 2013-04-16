855d7c3888099a1fa5282dfa6fce9d5fe0ad1e7d
renatominami <renatominami88@hotmail.com>
2013-04-16 15:42:39 -0300

    alteracao do gitignore e criacao do arquivo modelo versao 2

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 cd tiktak-api
-mvn clean install -DskipTests eclipse:eclipse
+mvn clean install -DskipTests eclipse:clean eclipse:eclipse
 
 cd ..
 cd simulacaoCliente
-mvn clean install -DskipTests eclipse:eclipse
+mvn clean install -DskipTests eclipse:clean eclipse:eclipse
 
 cd ..
 cd tiktak-dashboard
-mvn clean install -DskipTests eclipse:eclipse
+mvn clean install -DskipTests eclipse:clean eclipse:eclipse
