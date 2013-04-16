26fa15d02c99cbeeaae5eac8f1ff5869718ed5a5
Albert De La Fuente <vonpupp@gmail.com>
2013-04-16 16:04:57 -0300

    Ignora eventos repetidos (mesmo id)

--- tiktak-dashboard/web/src/main/java/br/org/tiktak/dashboard/pages/tabela/Tabela.java
+++ tiktak-dashboard/web/src/main/java/br/org/tiktak/dashboard/pages/tabela/Tabela.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import jmine.tec.web.wicket.pages.Template;
 
@@ -30,7 +31,7 @@ import com.google.gson.reflect.TypeToken;
 public class Tabela extends Template {
 
 	List<BDfuncionalidades> listaFuncionalidades = new ArrayList<BDfuncionalidades>();
-	Set<String> listaDeIds = new HashSet<String>();
+	Set<UUID> listaDeIds = new HashSet<UUID>();
 	HashMap<String, Integer> mapa = new HashMap<String, Integer>();
 	Integer totalDeEventos = 0;
 
@@ -85,10 +86,13 @@ public class Tabela extends Template {
 		List<Evento> lista = GsonFactory.getGson().fromJson(reader, new TypeToken<List<Evento>>() {
 		}.getType());
 		for (Evento evento : lista) {
-			totalDeEventos++;
-			String funcionalidade = evento.getFuncionalidade();
-			int count = mapa.containsKey(funcionalidade) ? mapa.get(funcionalidade) : 0;
-			mapa.put(funcionalidade, count + 1);
+			if(!listaDeIds.contains(evento.getUuid())) {
+				listaDeIds.add(evento.getUuid());
+				totalDeEventos++;
+				String funcionalidade = evento.getFuncionalidade();
+				int count = mapa.containsKey(funcionalidade) ? mapa.get(funcionalidade) : 0;
+				mapa.put(funcionalidade, count + 1);
+			}
 		}
 		listaFuncionalidades.clear();
         Set<String> setFuncionalidades = mapa.keySet();  
