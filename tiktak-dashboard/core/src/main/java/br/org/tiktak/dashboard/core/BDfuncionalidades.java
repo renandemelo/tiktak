44c412b354113b9b6a6704963e6d82371fb7e971
claynon + leonardo <claynon@gmail.com>
2013-04-25 14:54:36 -0300

    Começou a adicionar o campo sistema no dashboard. Foi feito pouca coisa porque depende das mudanças na camada de visão

--- tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/BDfuncionalidades.java
+++ tiktak-dashboard/core/src/main/java/br/org/tiktak/dashboard/core/BDfuncionalidades.java
@@ -3,14 +3,22 @@ package br.org.tiktak.dashboard.core;
 import java.io.Serializable;
 
 public class BDfuncionalidades implements Serializable {
+	private String sistema;
 	private String funcionalidade;
 	private int quantidade;
 	private String porcentagemFormatada;
 	
-	public BDfuncionalidades(String funcionalidade, int quantidade, String porcentagemFormatada) {
+	
+	public BDfuncionalidades(/*String sistema, */String funcionalidade, int quantidade, String porcentagemFormatada) {
+		//Depende da visualização estar modificada pra adicionar o sistema conforme necessário
+		//this.sistema = sistema;
 		this.funcionalidade = funcionalidade;
 		this.quantidade = quantidade;
 		this.porcentagemFormatada = porcentagemFormatada; 
+	}
+	
+	public String getSistema() {
+		return sistema;
 	}
 	
 	public String getPorcetagem() {
