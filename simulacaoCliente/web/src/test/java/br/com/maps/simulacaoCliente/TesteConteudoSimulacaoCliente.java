dd70f2c23b9be300d5a8bb7cad4c543c6d54828c
Leonardo Santana Oliveira <leo.oliveira@europa.eclipse.ime.usp.br>
2013-04-25 15:59:03 -0300

    piloto@leoso copiloto@claynon Arrumando o teste TesteConteudoSimulacaoCliente do selenium

--- simulacaoCliente/web/src/test/java/br/com/maps/simulacaoCliente/TesteConteudoSimulacaoCliente.java
+++ simulacaoCliente/web/src/test/java/br/com/maps/simulacaoCliente/TesteConteudoSimulacaoCliente.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -15,21 +16,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+/**
+ * Testa se o conteudo do arquivo coincide com os dados colocados na tela em um determinado evento.
+ * obs: supõe que a aplicação simulacaoCliente esteja "em pé".
+ * @author leo.oliveira
+ *
+ */
 public class TesteConteudoSimulacaoCliente {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://localhost:8888";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost:8888";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-	public void testE() throws Exception {
+	@Test
+	public void testeVerificaConteudoDoArquivoNoDiretorioPadrao() throws Exception {
 		String usuario = "ALBERT" + System.currentTimeMillis();
 		String funcionalidade = "TESTE-API-NOVO" + System.currentTimeMillis();
 		driver.get(baseUrl + "/login?1");
@@ -39,67 +46,38 @@ public class TesteConteudoSimulacaoCliente {
 		driver.findElement(By.name("password")).sendKeys("admin");
 		driver.findElement(By.id("botaoLogin")).click();
 		driver.findElement(By.id("menuLista")).click();
-		// Isto precisa ser arrumado, por algum motivo,
-		// quando clicamos no menu principal ele ja vai
-		// direto na tela menu.simulação.tela_de_simulação
-		
-		//driver.findElement(By.id("menu.simulação")).click();
-		//driver.findElement(By.className("submenu_1column")).click();
-		//driver.findElement(By.id("menu.simulação.tela_de_simulação")).click();
 		driver.findElement(By.name("txt_usuario")).clear();
 		driver.findElement(By.name("txt_usuario")).sendKeys(usuario);
 		driver.findElement(By.name("txt_funcionalidade")).clear();
 		driver.findElement(By.name("txt_funcionalidade")).sendKeys(funcionalidade);
 		driver.findElement(By.xpath("//input[@value='Enviar']")).click();
-		String conteudoArquivo = carrega("../tiktak/tik.tak");
+		String conteudoArquivo = carrega("tik.tak");
 		Boolean resultado = conteudoArquivo.contains(usuario);
 		assertTrue(conteudoArquivo.contains(usuario));
 		assertTrue(conteudoArquivo.contains(funcionalidade));
 	}
 
-  private String carrega(String arquivo) throws IOException {
-	// TODO Auto-generated method stub
-    BufferedReader reader = new BufferedReader( new FileReader (arquivo));
-    String line = null;
-    StringBuilder stringBuilder = new StringBuilder();
-    String ls = System.getProperty("line.separator");
+	private String carrega(String arquivo) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader( new FileReader (arquivo));
+		String line = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		String ls = System.getProperty("line.separator");
 
-    while ((line = reader.readLine()) != null ) {
-        stringBuilder.append(line);
-        stringBuilder.append(ls);
-    }
-    return stringBuilder.toString();
-}
+		while ((line = reader.readLine()) != null ) {
+			stringBuilder.append(line);
+			stringBuilder.append(ls);
+		}
+		return stringBuilder.toString();
+	}
 
-@After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-//  private boolean isElementPresent(By by) {
-//    try {
-//      driver.findElement(by);
-//      return true;
-//    } catch (NoSuchElementException e) {
-//      return false;
-//    }
-//  }
-//
-//  private String closeAlertAndGetItsText() {
-//    try {
-//      Alert alert = driver.switchTo().alert();
-//      if (acceptNextAlert) {
-//        alert.accept();
-//      } else {
-//        alert.dismiss();
-//      }
-//      return alert.getText();
-//    } finally {
-//      acceptNextAlert = true;
-//    }
-//  }
 }
