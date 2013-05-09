2510bdd785d606955bd737c4c7ad6bd84d06a380
Wallace <wallace.almeida89@gmail.com>
2013-05-09 15:03:43 -0300

    piloto @walmeida copiloto @claynon - Alterando novamente o formato do json e permitindo multi-sistemas

--- tiktak-api/src/main/java/br/org/tiktak/core/Eventv2.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/Eventv2.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Eventv2 {
 	private static Eventv2 instanciaSingleton;
-	private String system;
-	private ArrayList<Event> events;
+	private String sistema;
+	private ArrayList<Event> eventos;
 
 	public String getSystem() {
-		return system;
+		return sistema;
 	}
 
 	public ArrayList<Event> getEvent() {
-		return events;
+		return eventos;
 	}
 	
     // Método público estático de acesso único ao objeto!
@@ -26,18 +26,18 @@ public class Eventv2 {
     // Construtor privado. Suprime o construtor público padrao.
     private Eventv2() {
          // Operações de inicialização da classe
-		this.system = null;
-		this.events = new ArrayList<Event>();
+		this.sistema = null;
+		this.eventos = new ArrayList<Event>();
     }
     
     // Construtor privado. Suprime o construtor público padrao.
     public void Init(final String system) {
          // Operações de inicialização da classe
-		this.system = system;
+		this.sistema = system;
     }
 
 	public void log(final String user, final String funcionality) {
 		Event event = new Event(user, funcionality);
-		this.events.add(event);
+		this.eventos.add(event);
 	}
 }
